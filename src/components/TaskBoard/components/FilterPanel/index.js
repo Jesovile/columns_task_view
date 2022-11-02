22e0d41d570cb8a43fb943fb328c52c18268af47
Stepan Saprunov <saprunovstepan.0@gmail.com>
2022-11-03 01:32:19 +0400

    added debounce to filter input

--- src/components/TaskBoard/components/FilterPanel/index.js
+++ src/components/TaskBoard/components/FilterPanel/index.js
@@ -1,22 +1,40 @@
-import React, {useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './FilterPanel.css';
 
+
+
 const FilterPanel = (props) => {
-    const {onChangeFilter} = props;
+    const { onChangeFilter, debounceTimer = 1000 } = props;
     const [filterParams, setFilterParams] = useState({});
     const onChangeParam = (value, name) => {
-        const newFilters = {...filterParams, [name]: value};
+        const newFilters = { ...filterParams, [name]: value };
         setFilterParams(newFilters);
     }
+
     const onSearch = () => {
         onChangeFilter && onChangeFilter(filterParams);
     }
+
+    let lastCall = Date.now();
+    let lastCallTimer;
+
+    const debounce = useCallback(function (f, t) {
+        return function (args) {
+            let previousCall = lastCall;
+            lastCall = Date.now();
+            if (previousCall && (lastCall - previousCall <= t)) {
+                clearTimeout(lastCallTimer);
+            }
+            lastCallTimer = setTimeout(()=>{f(args)}, t);
+        }
+    }, []);
+
+    useEffect(debounce(onSearch, debounceTimer), [filterParams])
 
     return (
         <div className="FilterPanel">
             <label>By Title:<input value={filterParams.title} type={"text"} onChange={(e) => onChangeParam(e.target.value, 'title')}/>
             </label>
-            <button onClick={onSearch}>Search</button>
         </div>
     );
 }
